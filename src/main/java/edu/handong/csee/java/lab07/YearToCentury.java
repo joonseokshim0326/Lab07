14cb8935673ba282fdd8d8bffaf9091c47344f4d
Jsshim <21300418@handong.edu>
2018-04-04 13:34:02 +0900

    second commit(solve problem)

--- src/main/java/edu/handong/csee/java/lab07/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/YearToCentury.java
@@ -1,10 +1,16 @@
-package edu.handong.csee.java.lab07;
+package edu.handong.csee.java.lab07;// defining package which our class is in
 
-import java.util.Scanner;
+import java.util.Scanner;// Gets the Scanner class from the package(library)java.util
 
-public class YearToCentury {
-
-	int year;
+/**
+ * Class YearTocentury is desinged to calculates the century given a year
+ * Using class YearTocentury 1900 is 19th century and 1967 is 20th century
+ * @author jssjp
+ * @since 20180405
+ */
+public class YearToCentury  // defining class Year To century
+{
+	int year; // variable statement
 	
 	public YearToCentury()
 	{
